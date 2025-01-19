4102bbff07a0848260da5c3b87e6e7769ee7143e
vinybelli <vinybelli@hotmail.com>
2025-01-19 18:14:58 -0300

    Correction of an index on the new payment method

--- Lava Car/Cadastros/Pedidos/Cadastro_Pedido.cs
+++ Lava Car/Cadastros/Pedidos/Cadastro_Pedido.cs
@@ -470,7 +470,7 @@ namespace Lava_Car.Cadastros.Pedidos
 
         private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBox4.SelectedIndex == 7) 
+            if (comboBox4.SelectedIndex == 6) 
             { 
                 textBox3.Text = "0,00";
             }
